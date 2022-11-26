ce9f62d62753c89e030eef3e2b585ea3fafe37c5
Christian Mulato <chmulato@hotmail.com>
2022-11-26 16:54:37 -0300

    Remover Curso: API Spring HTTP DELETE

--- src/main/java/com/mulato/controller/CourseController.java
+++ src/main/java/com/mulato/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,4 +58,14 @@ public class CourseController {
             .orElse(ResponseEntity.notFound().build());
 
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        return courseRepository.findById(id)
+        .map(courseFound -> {
+            courseRepository.delete(courseFound);
+            return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
+}
 }
